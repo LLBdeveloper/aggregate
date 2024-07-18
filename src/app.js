85d80f33671725c5995c2fefdeabdb03baddbb7e
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-07-18 11:27:16 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+import OrderModel from "./models/order.model.js"
+
+const main = async () => {
+    mongoose.connect("mongodb+srv://LLBdeveloper:admiadmi@cluster0.kdv9gnv.mongodb.net/Pizzalandia?retryWrites=true&w=majority&appName=Cluster0")
+
+    const resultado = await OrderModel.aggregate([
+        {
+            $match: {
+                tam: "chica"
+            }
+        },
+        {
+            $group: {
+                _id: "$nombre",
+                total: {
+                    $sum: "$cantidad"
+                }
+            }
+        },
+        {
+            $sort: {
+                total: 1
+            }
+        },
+        {
+            $group: {
+                _id: 10,
+                orders: {
+                    $push: "$$ROOT"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                orders: "$orders"
+            }
+        },
+        {
+            $merge: {
+                into: "reports"
+            }
+        }
+    ])
+    console.log(resultado)
+}
+
+main()
